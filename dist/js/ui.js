dbab4ae520cd6f29095089b8b4fa31c09463f595
Andrew M <32660013+mikandrew53@users.noreply.github.com>
2020-01-07 08:09:35 -0500

    Update ui.js

--- dist/js/ui.js
+++ dist/js/ui.js
@@ -2,16 +2,19 @@ class UI {
     constructor (){
         this.sort = {
             description: '<p>Built an application that visualizes popular sorting algorithms. I implemented merge sort, quick sort, and bubble sort. Spacetime complexity was kept in mind when writing the algorithms.</p>',
-            github: 'https://github.com/mikandrew53/Sorting-Visualizer'
+            github: 'https://github.com/mikandrew53/Sorting-Visualizer',
+            header: 'Sorting Visualizer'
 
         };
         this.weather = {
             description: '<p>Built a weather app to display current and past weather for various cities using the ‘metaweather’ api. The app is optimized o make the minimum amount of http requests in order to decrease traffic on the backend server.</p>',
-            github: 'https://github.com/mikandrew53/Weather-App'
+            github: 'https://github.com/mikandrew53/Weather-App',
+            header: 'Weather App'
         }
         this.shop = {
             description: '<p>Built an online store that with a dynamically updating shopping cart. Implemented a rounding class to round the cost of the items in the cart</p>',
-            github: 'https://github.com/mikandrew53/Online-Shoping-Store'
+            github: 'https://github.com/mikandrew53/Online-Shoping-Store',
+            header: 'Online Shopping Store'
         }
         this.sortingModal = document.getElementById('sorting-modal');
         this.sortLink = '#';
@@ -32,13 +35,12 @@ class UI {
     }
 
     paintModalBody(project){
+        document.querySelector('.title').innerHTML = project.header;
         document.querySelector('.modal-body').innerHTML = `
         <div>${project.description}</div>
         <div class = modal-btn>
             <a href="" class="btn-dark" target="_blank">Live Demo</a>
             <a href="${project.github}" class="btn-dark" target="_blank">Source Code</a>
-        </div>
-            `;
-
+        </div>`;
     }
 }
